72d20235c65b3ed3038f5155cb6956c6a948debe
Kyungjae Jang <economyjang777@gmail.com>
2024-01-25 21:12:37 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,14 @@
+// 3명을 더해서 0이 되야만 삼총사
+function solution(number) {
+    let answer = 0;
+    
+    for(let i = 0; i < number.length; i++) {
+        for(let j = i + 1; j < number.length; j++) {
+            for(let k = j + 1; k < number.length; k++) {
+                if(number[i] + number[j] + number[k] === 0) answer++;
+            }
+        }
+    }
+    
+    return answer;
+}
