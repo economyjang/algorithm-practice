dd00428ecf876570f5894ca9077d7112d04f36a3
Kyungjae Jang <economyjang777@gmail.com>
2024-01-25 15:12:00 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.12 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,13 +1,8 @@
 function solution(left, right) {
     let sum = 0;
     for(let i = left; i <= right; i++) {
-        let count = 0;
-        for(let j = 1; j <= i; j++) {
-            if(i % j === 0) count++;
-        }
-        
-        if(count % 2 === 0) sum += i;
-        else sum -= i;
+        if(Number.isInteger(Math.sqrt(i))) sum -= i;
+        else sum += i;
     }
     
     return sum;
