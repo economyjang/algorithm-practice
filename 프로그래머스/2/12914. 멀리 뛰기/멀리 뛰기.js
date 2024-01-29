3652063f087d64a5e979bd081cab7801b2b670f0
Kyungjae Jang <economyjang777@gmail.com>
2024-01-29 17:43:41 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let dp = {};
+    
+    dp[0] = 1;
+    dp[1] = 2;
+    
+    for(let i = 2; i <= n - 1; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+    
+    return dp[n - 1];
+}
