ccab1ccecf159727cd4eb2f1ee572fc1c417a200
Kyungjae Jang <economyjang777@gmail.com>
2024-02-08 13:08:54 +0900

    [level 2] Title: 할인 행사, Time: 0.23 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,18 @@
+function solution(want, number, discount) {
+    let answer = 0;
+    
+    const wantMap = {};
+    want.forEach((item, idx) => wantMap[item] = number[idx]);
+    
+    for(let i = 0; i < discount.length; i++) {
+        const subArr = discount.slice(i, i + 10);
+        
+        const subArrMap = {};
+        subArr.forEach((item) => subArrMap[item] = subArrMap[item] ? subArrMap[item] + 1 : 1);
+        
+        
+        if(Object.keys(wantMap).filter((key) => wantMap[key] !== subArrMap[key]).length === 0) answer++;
+    }
+    
+    return answer;
+}
