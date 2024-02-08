52d153d1b8653c87bacb8e3e3c206b2a65db8a1c
Kyungjae Jang <economyjang777@gmail.com>
2024-02-08 13:59:04 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.42 ms, Memory: 44 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -0,0 +1,17 @@
+function solution(n, left, right) {
+    const answer = [];
+    
+    for(let i = left; i <= right; i++){
+        const divide = Math.floor(i / n);
+        const rest = i % n;
+        
+        if(divide >= rest){
+            answer.push(divide + 1);
+        }
+        else{
+            answer.push(rest + 1);
+        }
+    }
+    
+    return answer;
+}
